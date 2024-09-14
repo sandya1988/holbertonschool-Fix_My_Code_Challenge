573ee6fe87365a3a94b7391e6bfe425c5ff9dbe5
Sandya1988 <Aoun.sandy@gmail.com>
2024-09-14 22:48:46 +0300

    dd

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -32,9 +32,8 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
+        print("Example: ./0-fizzbuzz.py 50")
         sys.exit(1)
 
     number = int(sys.argv[1])
     fizzbuzz(number)
-    
